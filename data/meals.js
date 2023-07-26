030a8e8fb302e852b8f539925585a779aa631fb3
Ecoman5 <olatunbosunadelana56@gmail.com>
2023-07-26 15:29:54 +0100

    leaning express

---
+++ data/meals.js
@@ -0,0 +1,25 @@
+const meals = [
+ {
+  id: 1,
+  name: "Rice Noodles",
+ },
+ {
+  id: 2,
+  name: "Yam porriage",
+ },
+ {
+  id: 3,
+  name: "Fried Rice",
+ },
+  {
+  id: 4,
+  name: "Jollof Rice",
+ },
+  {
+  id: 5,
+  name: "Salad Rice",
+ },
+ 
+]
+
+module.exports = meals
